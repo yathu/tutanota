f8dea4cb0078d9cd7200bffcfa249f05bd24ec99
yathavan <yathavancom@gmail.com>
2023-05-25 16:33:08 +0200

    optimized

--- app.js
+++ app.js
@@ -1,37 +1,56 @@
-const urlPatternRegx = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+const urlPatternRegx = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+;
+
+//for Typescript, we can use this
+// enum URLType {
+//     File = "file",
+//     Folder = "folder"
+// }
+
+// type URLObject = {
+//     url: string;
+//     type: URLType;
+// };
+
 const URL_List = [
-    "https://www.google.com",
-    "https://www.wikipedia.org",
-    "https://www.openai.com",
-    "https://www.github.com",
-    "https://www.stackoverflow.com"
+    {url: "https://www.google.com", type: "file"},
+    {url: "https://www.wikipedia.org", type: "folder"},
+    {url: "https://www.openai.com", type: "file"},
+    {url: "https://www.github.com", type: "folder"},
+    {url: "https://www.stackoverflow.com", type: "file"}
 ];
 
-//default timeout 1s
-const debounce = (func, wait = 1000) => {
-    let timeout;
-    return function executedFunction(...args) {
-        const later = () => {
-            clearTimeout(timeout);
-            func(...args);
-        };
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-    };
+//debounce to avoid throttle
+let timeout;
+const debounce = function (func, delay = 300) {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, delay);
 };
 
 //get value from input & check
-document.getElementById("urlInput").addEventListener("input", async function (e) {
+document.getElementById("urlInput").addEventListener("input", function (e) {
     const url = e.target.value.trim();
     const isValid = isValidURL(url);
     this.classList.add(isValid ? 'border-green-500' : 'border-red-300');
     this.classList.remove(!isValid ? 'border-green-500' : 'border-red-300');
 
     if (isValid) {
-       debounceCheck(url);
+        debounce(() => checkUrlType(url), 300);
     }
 
 });
+
+const checkUrlType = async (url) => {
+    await ApiCall(url).then((type) => {
+        if (type) {
+            document.getElementById('file-container').classList.remove('hidden');
+            document.getElementById('filetype').innerHTML = '';
+            document.getElementById('filetype').append(type);
+        } else {
+            document.getElementById('file-container').classList.add('hidden');
+        }
+    });
+}
 
 //url validation
 const isValidURL = string => {
@@ -39,21 +58,14 @@ const isValidURL = string => {
     return (result !== null);
 }
 
-//mock check is exists using debounce to avoid multiple request
-const debounceCheck = debounce(async function (str) {
+//mock API call Async
+const ApiCall = async (str) => {
     const res = await new Promise(resolve => setTimeout(() => {
-        resolve(URL_List.includes(str))
-    }, 100));
+        //file ths url & return the object
+        const urlObject = URL_List.find(obj => obj.url == str);
+        const urlType = urlObject ? urlObject.type : null;
+        resolve(urlType);
+    }, 0));
 
-
-    if(res) {
-        //show error message
-        document.getElementById('urlError').classList.add('block');
-        document.getElementById('urlError').classList.remove('hidden');
-    }else {
-        //can Save Data
-        document.getElementById('urlError').classList.remove('block');
-        document.getElementById('urlError').classList.add('hidden');
-    }
-
-}, 500);
+    return res;
+}
